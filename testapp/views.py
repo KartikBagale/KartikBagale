0fc378ae910c351b22de1cefb97039c7bdbd34ac
KartikBagale <bagalekartik22@gmail.com>
2021-12-26 10:40:39 +0530

    First ModelViewSet

---
+++ testapp/views.py
@@ -0,0 +1,11 @@
+from django.db.models.query import QuerySet
+from django.shortcuts import render
+from . models import Employee
+from . serializers import EmployeeSerializer
+from rest_framework import viewsets
+
+# Create your views here.
+
+class EmployeeModelViewSet(viewsets.ModelViewSet):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
